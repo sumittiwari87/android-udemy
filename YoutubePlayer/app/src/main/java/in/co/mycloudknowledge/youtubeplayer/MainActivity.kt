81ff4fbbcd7b54230726e2f1a7a665f2a6526860
Sumit <sumit_tiwari@gmail.com>
2020-02-27 20:01:02 -0300

    Set up

---
+++ YoutubePlayer/app/src/main/java/in/co/mycloudknowledge/youtubeplayer/MainActivity.kt
@@ -0,0 +1,12 @@
+package `in`.co.mycloudknowledge.youtubeplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
